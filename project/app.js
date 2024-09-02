b09d5571e533b41f47f2ce6eac3b9abcf5d2adf6
Nithisha-gif <nithisha2805@gmail.com>
2024-09-02 15:02:04 +0530

    app.js

--- project/app.js
+++ project/app.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import Navbar from './components/Navbar';
 
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div>
+      <Navbar />
+      {isLoggedIn ? (
+        <HomePage />
+      ) : (
+        <LoginPage onLogin={() => setIsLoggedIn(true)} />
+      )}
+    </div>
+  );
+}
+
+export default App;
